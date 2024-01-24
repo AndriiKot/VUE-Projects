0459290fcfcd96671ea35bb154e75b4927888d33
AndriiKot <a_kocyuba@ukr.net>
2024-01-24 09:53:42 +0100

    add methods vue

--- Vue learning/vue_learning_ua/src/MyComponent.vue
+++ Vue learning/vue_learning_ua/src/MyComponent.vue
@@ -1,14 +1,25 @@
 <template>
   <h2>Счётчик: {{ counter }}</h2>
-  <button class="btn btn-danger" v-on:click="counter--">Уменьшить</button>
-  <button class="btn btn-success" v-on:click="counter++">Увеличить</button>
+  <p>max: 10, mix: 0</p>
+  <button class="btn btn-danger" v-on:click="minus">Уменьшить</button>
+  <button class="btn btn-success" v-on:click="plus">Увеличить</button>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        counter: 0
+        counter: 1
+      }
+    },
+    methods: {
+      plus(){
+        if(this.counter === 10) { return }
+        this.counter += 1
+      },
+      minus() {
+        if(this.counter === 0) { return }
+        this.counter -= 1
       }
     }
   }
