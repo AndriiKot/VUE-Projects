a3c8c8c814c9c506a1e959e5a3be5e3afe6d8465
AndriiKot <a_kocyuba@ukr.net>
2024-01-24 09:09:12 +0100

    add buttons

--- Vue learning/vue_learning_ua/src/App.vue
+++ Vue learning/vue_learning_ua/src/App.vue
@@ -1,17 +1,7 @@
 <template>
-  <h1>Year: {{year}}</h1>
   <my-component />
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        year: 2024
-      }
-    }
-  }
-</script>
 
 <style>
 </style>
--- Vue learning/vue_learning_ua/src/MyComponent.vue
+++ Vue learning/vue_learning_ua/src/MyComponent.vue
@@ -1,3 +1,16 @@
 <template>
-  <h2>My Component</h2>
+  <h2>Счётчик: {{ counter }}</h2>
+  <button class="btn btn-danger" v-on:click="counter--">Уменьшить</button>
+  <button class="btn btn-success" v-on:click="counter++">Увеличить</button>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0
+      }
+    }
+  }
+</script>
+
